2e29a16e608cc688215d2d94522881c3ade75131
Uksus149 <118109344+Uksus149@users.noreply.github.com>
2026-05-20 20:30:03 +0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,14 +1,14 @@
 
 import { createApp } from 'vue'; 
 import App from './App.vue'; 
-import { createRouter, createWebHistory } from 'vue-router'; 
+import { createRouter, createWebHistory, createWebHashHistory  } from 'vue-router'; 
 
 
 import { routes } from './route.js';
 
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes,
 });
 
